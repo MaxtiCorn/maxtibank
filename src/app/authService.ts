f7839ca57c134df28e77c8f29916d6b3cd6d80d1
maxticorn <letunovskeymax@gmail.com>
2019-01-16 17:22:17 +0500

    api endpoint

--- src/app/authService.ts
+++ src/app/authService.ts
@@ -1,6 +1,7 @@
 import { User } from "./user";
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { AppConfig } from "./appConfig";
 
 @Injectable({
     providedIn: 'root',
@@ -11,7 +12,7 @@ export class AuthService {
     constructor(private readonly httpService: HttpClient) { }
 
     auth(login: string, password: string) {
-        this.httpService.post('http://localhost:6969/auth', { login: login, password: password })
+        this.httpService.post(AppConfig.API_ENDPOINT + '/auth', { login: login, password: password })
             .subscribe((res) => {
                 if (res['success']) {
                     this.user = new User(login);
--- src/app/operService.ts
+++ src/app/operService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Operation } from "./operation";
+import { AppConfig } from "./appConfig";
 
 @Injectable({
     providedIn: 'root',
@@ -9,7 +10,7 @@ export class OperService {
     constructor(private readonly httpService: HttpClient) { }
 
     addOperation(operation: Operation) {
-        this.httpService.post('http://localhost:6969/addOperation', operation)
+        this.httpService.post(AppConfig.API_ENDPOINT + '/addOperation', operation)
             .subscribe((response) => {
                 response['success']
             });
